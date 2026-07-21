f853ab87dfc0cd683824d337821ee5639d234fbc
Justin Riley <20hz20khz@gmail.com>
2026-07-20 20:20:18 -0400

    Create color_schemes.py

---
+++ color_schemes.py
@@ -0,0 +1,302 @@
+LIGHT_COLOR_SCHEME = {
+    "canvas_color":"#f4f4f0",
+    "track_color": "#ffddff",
+    "text_color": "#342A21",
+    "border_color": "#783F8E",
+    "muted_text_color": "#50C9CE",
+    "track_width":12,
+    "font_path": "/System/Library/Fonts/Arial Bold.ttf"
+}
+DARK_COLOR_SCHEME = {
+    "canvas_color":"#212121",
+    "track_color": "#6C006C",
+    "text_color": "#F9F5FF",
+    "border_color": "#9882AC",
+    "muted_text_color": "#7B7554",
+    "track_width":8,
+    "font_path": "/System/Library/Fonts/Avenir Next.ttc"
+}
+TROPICAL_BLISS_COLOR_SCHEME = {
+    "canvas_color": "#fef9ef",
+    "track_color": "#17c3b2",
+    "text_color": "#18206F",
+    "border_color": "#fe6d73",
+    "muted_text_color": "#D7B377",
+    "track_width":10,
+    "font_path": "/System/Library/Fonts/Times New Roman Bold.ttf"
+}
+CYBERPUNK_COLOR_SCHEME = {
+    "canvas_color": "#0d0e15",
+    "track_color": "#00ff66",
+    "text_color": "#FCFFFD",
+    "border_color": "#6320EE",
+    "muted_text_color": "#6320EE",
+    "track_width":4,
+    "font_path": "/System/Library/Fonts/Trebuchet MS Bold.ttf"
+}
+PASTEL_COLOR_SCHEME = {
+    "canvas_color": "#fffdf0",
+    "track_color": "#ffd3f3",
+    "text_color": "#3d263a",
+    "border_color": "#9bf6ff",
+    "muted_text_color": "#B5EF8A",
+    "track_width":10,
+    "font_path": "/System/Library/Fonts/Arial Rounded Bold.ttf"
+}
+DRACULA_COLOR_SCHEME = {
+    "canvas_color": "#1e1f29",
+    "track_color": "#ff5555",
+    "text_color": "#f8f8f2",
+    "border_color": "#bd93f9",
+    "muted_text_color": "#6272a4",
+    "track_width": 6,
+    "font_path": "/System/Library/Fonts/HelveticaNeue.ttc"
+}
+DESERT_OASIS_COLOR_SCHEME = {
+    "canvas_color": "#fbf6ef",
+    "track_color": "#daa191",
+    "text_color": "#012a40",
+    "border_color": "#81b29a",
+    "muted_text_color": "#b8b0a6",
+    "track_width": 10,
+    "font_path": "/System/Library/Fonts/Supplemental/Microsoft Sans Serif.ttf"
+}
+DEEP_OCEAN_COLOR_SCHEME = {
+    "canvas_color": "#0b132b",
+    "track_color": "#00b4d8",
+    "text_color": "#FFF1D0",
+    "border_color": "#EF946C",
+    "muted_text_color": "#5c677d",
+    "track_width": 8,
+    "font_path": "/System/Library/Fonts/Futura.ttc"
+}
+MINT_COLOR_SCHEME = {
+    "canvas_color":"#f0f4f4",
+    "track_color": "#50C9CE",
+    "text_color": "#3D1D00",
+    "border_color": "#783F8E",
+    "muted_text_color": "#E5A4FF",
+    "track_width":12,
+    "font_path": "/System/Library/Fonts/Arial Bold.ttf"
+}
+GOLDENROD_COLOR_SCHEME = {
+    "canvas_color":"#212121",
+    "track_color": "#DAA520",
+    "text_color": "#E5A4FF",
+    "border_color": "#9882AC",
+    "muted_text_color": "#6C006C",
+    "track_width":8,
+    "font_path": "/System/Library/Fonts/Avenir Next.ttc"
+}
+SALMON_COLOR_SCHEME = {
+    "canvas_color": "#ecfeff",
+    "track_color": "#fe6d73",
+    "text_color": "#18206F",
+    "border_color": "#17c3b2",
+    "muted_text_color": "#D7B377",
+    "track_width":10,
+    "font_path": "/System/Library/Fonts/Times New Roman Bold.ttf"
+}
+ELECTRIC_COLOR_SCHEME = {
+    "canvas_color": "#0d0e15",
+    "track_color": "#7DF9FF" ,
+    "text_color": "#FF00FF",
+    "border_color": "#CCFF00",
+    "muted_text_color": "#356a4a",
+    "track_width":6,
+    "font_path": "/System/Library/Fonts/Trebuchet MS Bold.ttf"
+}
+MIAMI_COLOR_SCHEME = {
+    "canvas_color": "#ffecfc",
+    "track_color": "#C1FF9B",
+    "text_color": "#2E5266",
+    "border_color": "#0ad2d3",
+    "muted_text_color": "#f990e8",
+    "track_width":12,
+    "font_path": "/System/Library/Fonts/Arial Rounded Bold.ttf"
+}
+GOTHIC_LAVENDER_COLOR_SCHEME = {
+    "canvas_color": "#1e1f29",
+    "track_color": "#bd93f9",
+    "text_color": "#cdf5ff",
+    "border_color": "#FFECFC",
+    "muted_text_color": "#8E4162",
+    "track_width": 6,
+    "font_path": "/System/Library/Fonts/HelveticaNeue.ttc"
+}
+LIFE_AQUATIC_COLOR_SCHEME = {
+    "canvas_color": "#F4EBDE",
+    "track_color": "#35b8ff",
+    "text_color": "#8a1e00",
+    "border_color": "#4C9A9A",
+    "muted_text_color": "#b8b0a6",
+    "track_width": 10,
+    "font_path": "/System/Library/Fonts/Supplemental/Microsoft Sans Serif.ttf"
+}
+FRANKENBERRY_COLOR_SCHEME = {
+    "canvas_color": "#0b132b",
+    "track_color": "#e5989c",
+    "text_color": "#FFECFC",
+    "border_color": "#7F2CCB",
+    "muted_text_color": "#5c677d",
+    "track_width": 8,
+    "font_path": "/System/Library/Fonts/Futura.ttc"
+}
+REDS_LIGHT_COLOR_SCHEME = {
+    "canvas_color": "#CDD6D0",
+    "track_color": "#E60000",
+    "text_color": "#740000",
+    "border_color": "#ffa2a2",
+    "muted_text_color": "#ffa2a2",
+    "track_width": 6,
+    "font_path": "/System/Library/Fonts/Arial Bold.ttf"
+}
+ORANGES_DARK_COLOR_SCHEME = {
+    "canvas_color": "#714220",
+    "track_color": "#ff3503",
+    "text_color": "#ffae9a",
+    "border_color": "#AE8E1C",
+    "muted_text_color": "#AE8E1C",
+    "track_width": 10,
+    "font_path": "/System/Library/Fonts/Avenir Next.ttc"
+}
+YELLOWS_LIGHT_COLOR_SCHEME = {
+    "canvas_color": "#f0e3d2",
+    "track_color": "#fffc00",
+    "text_color": "#56351E",
+    "border_color": "#F4BB00",
+    "muted_text_color": "#F4BB00",
+    "track_width": 12,
+    "font_path": "/System/Library/Fonts/Trebuchet MS Bold.ttf"
+}
+GREENS_DARK_COLOR_SCHEME = {
+    "canvas_color": "#0B2E00",
+    "track_color": "#62FF00",
+    "text_color": "#D7FFCC",
+    "border_color": "#2D8000",
+    "muted_text_color": "#2D8000",
+    "track_width": 8,
+    "font_path": "/System/Library/Fonts/Arial Rounded Bold.ttf"
+}
+BLUES_LIGHT_COLOR_SCHEME = {
+    "canvas_color": "#E6FEFF",
+    "track_color": "#7DF9FF",
+    "text_color": "#004447",
+    "border_color": "#7DB8FF",
+    "muted_text_color": "#7DB8FF",
+    "track_width": 10,
+    "font_path": "/System/Library/Fonts/HelveticaNeue.ttc"
+}
+INDIGOS_DARK_COLOR_SCHEME = {
+    "canvas_color": "#2c0066",
+    "track_color": "#6F00FF",
+    "text_color": "#f0e5ff",
+    "border_color": "#9a4cff",
+    "muted_text_color": "#9a4cff",
+    "track_width": 6,
+    "font_path": "/System/Library/Fonts/Supplemental/Microsoft Sans Serif.ttf"
+}
+VIOLETS_LIGHT_COLOR_SCHEME = {
+    "canvas_color": "#E6D9FF",
+    "track_color": "#8F00FF",
+    "text_color": "#340063",
+    "border_color": "#B78AFF",
+    "muted_text_color": "#B78AFF",
+    "track_width": 12,
+    "font_path": "/System/Library/Fonts/Futura.ttc"
+}
+REDS_DARK_COLOR_SCHEME = {
+    "canvas_color": "#400000",
+    "track_color": "#E60000",
+    "text_color": "#FFD8D1",
+    "border_color": "#730000",
+    "muted_text_color": "#730000",
+    "track_width": 6,
+    "font_path": "/System/Library/Fonts/Arial Bold.ttf"
+}
+ORANGES_LIGHT_COLOR_SCHEME = {
+    "canvas_color": "#FFDDD4",
+    "track_color": "#ff3503",
+    "text_color": "#480800",
+    "border_color": "#FF957D",
+    "muted_text_color": "#FF957D",
+    "track_width": 10,
+    "font_path": "/System/Library/Fonts/Avenir Next.ttc"
+}
+YELLOWS_DARK_COLOR_SCHEME = {
+    "canvas_color": "#2E2D00",
+    "track_color": "#fffc00",
+    "text_color": "#FEFFBE",
+    "border_color": "#807E00",
+    "muted_text_color": "#807E00",
+    "track_width": 8,
+    "font_path": "/System/Library/Fonts/Trebuchet MS Bold.ttf"
+}
+GREENS_LIGHT_COLOR_SCHEME = {
+    "canvas_color": "#E5FFDD",
+    "track_color": "#62FF00",
+    "text_color": "#0B2E00",
+    "border_color": "#7EB36F",
+    "muted_text_color": "#7EB36F",
+    "track_width": 8,
+    "font_path": "/System/Library/Fonts/Arial Rounded Bold.ttf"
+}
+BLUES_DARK_COLOR_SCHEME = {
+    "canvas_color": "#112C2E",
+    "track_color": "#7DF9FF",
+    "text_color": "#E9FEFF",
+    "border_color": "#3B7D80",
+    "muted_text_color": "#3B7D80",
+    "track_width": 10,
+    "font_path": "/System/Library/Fonts/HelveticaNeue.ttc"
+}
+INDIGOS_LIGHT_COLOR_SCHEME = {
+    "canvas_color": "#DCD9FF",
+    "track_color": "#6F00FF",
+    "text_color": "#0E002E",
+    "border_color": "#7670A9",
+    "muted_text_color": "#7670A9",
+    "track_width": 6,
+    "font_path": "/System/Library/Fonts/Supplemental/Microsoft Sans Serif.ttf"
+}
+VIOLETS_DARK_COLOR_SCHEME = {
+    "canvas_color": "#15002E",
+    "track_color": "#8F00FF",
+    "text_color": "#DAC6FF",
+    "border_color": "#8370A9",
+    "muted_text_color": "#8370A9",
+    "track_width": 12,
+    "font_path": "/System/Library/Fonts/Futura.ttc"
+}
+THEMES = [
+    REDS_LIGHT_COLOR_SCHEME,
+    ORANGES_DARK_COLOR_SCHEME,
+    YELLOWS_LIGHT_COLOR_SCHEME,
+    GREENS_DARK_COLOR_SCHEME,
+    BLUES_LIGHT_COLOR_SCHEME,
+    INDIGOS_DARK_COLOR_SCHEME,
+    VIOLETS_LIGHT_COLOR_SCHEME,
+    REDS_DARK_COLOR_SCHEME,
+    ORANGES_LIGHT_COLOR_SCHEME,
+    YELLOWS_DARK_COLOR_SCHEME,
+    GREENS_LIGHT_COLOR_SCHEME,
+    BLUES_DARK_COLOR_SCHEME,
+    INDIGOS_LIGHT_COLOR_SCHEME,
+    VIOLETS_DARK_COLOR_SCHEME,
+    LIGHT_COLOR_SCHEME,
+    DARK_COLOR_SCHEME,
+    CYBERPUNK_COLOR_SCHEME,
+    TROPICAL_BLISS_COLOR_SCHEME,
+    PASTEL_COLOR_SCHEME,
+    DRACULA_COLOR_SCHEME,
+    DESERT_OASIS_COLOR_SCHEME,
+    DEEP_OCEAN_COLOR_SCHEME,
+    MINT_COLOR_SCHEME,
+    GOLDENROD_COLOR_SCHEME,
+    SALMON_COLOR_SCHEME,
+    ELECTRIC_COLOR_SCHEME,
+    MIAMI_COLOR_SCHEME,
+    GOTHIC_LAVENDER_COLOR_SCHEME,
+    LIFE_AQUATIC_COLOR_SCHEME,
+    FRANKENBERRY_COLOR_SCHEME
+]
